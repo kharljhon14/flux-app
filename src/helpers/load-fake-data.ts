479f861c94e9ae05307acfb3d1974cef9c5d6407
Karl <enriquezkharl14@gmail.com>
2023-08-22 15:56:42 +0800

    db connection

--- src/helpers/load-fake-data.ts
+++ src/helpers/load-fake-data.ts
@@ -1,5 +1,23 @@
-function loadFakeData(numUsers: number = 10) {
+import { Client } from 'pg';
+
+async function loadFakeData(numUsers: number = 10) {
   console.log(`executing load fake data. generating ${numUsers} users.`);
+
+  const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'flux',
+    password: 'zheoden',
+    port: 5432,
+  });
+
+  await client.connect();
+
+  const res = await client.query('select 1');
+
+  console.log(res);
+
+  await client.end();
 }
 
 loadFakeData();
