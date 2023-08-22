2e53768157f145ccb7e77124544c6a8013c631f2
Karl <enriquezkharl14@gmail.com>
2023-08-22 15:42:14 +0800

    fake data helper

---
+++ src/helpers/load-fake-data.js
@@ -0,0 +1,5 @@
+function loadFakeData(numUsers) {
+    if (numUsers === void 0) { numUsers = 10; }
+    console.log("executing load fake data. generating ".concat(numUsers, " users."));
+}
+loadFakeData();
