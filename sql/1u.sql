db6c96915165a4ce788542686313f9c6eabe1562
Karl <enriquezkharl14@gmail.com>
2023-08-22 13:57:34 +0800

    init migration

---
+++ sql/1u.sql
@@ -0,0 +1,32 @@
+CREATE extension IF NOT EXISTS citext;
+
+CREATE TABLE IF NOT EXISTS public.users (
+    id bigserial PRIMARY KEY,
+    username citext UNIQUE NOT NULL,
+    password text,
+    avatar text,
+    is_admin boolean DEFAULT false,
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now()
+);
+
+CREATE TABLE if NOT EXISTS public.posts (
+    id bigserial PRIMARY KEY,
+    user_id bigint REFERENCES public.users(id),
+    content text,
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now()
+);
+
+CREATE TABLE if NOT EXISTS public.follows (
+    user_id bigint NOT NULL REFERENCES public.users(id),
+    follower_id bigint NOT NULL REFERENCES public.users(id),
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now()
+);
+
+CREATE INDEX post_user_id_index ON public.posts(user_id);
+
+CREATE INDEX follows_user_id_index ON public.follows(user_id);
+
+CREATE INDEX follows_follower_id_index ON public.follows(user_id);
