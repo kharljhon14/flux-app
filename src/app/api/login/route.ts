1afea3170db1dd9a43d73640f6998b4851b5ac4c
Karl <enriquezkharl14@gmail.com>
2023-08-23 11:22:06 +0800

    login endpoint

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,5 +1,8 @@
-import { getClient, sql } from '@/db';
+import { sql } from '@/db';
+import { compare } from 'bcrypt';
 import { NextResponse } from 'next/server';
+
+import { SignJWT } from 'jose';
 
 export async function POST(request: Request) {
   const json = await request.json();
@@ -8,6 +11,25 @@ export async function POST(request: Request) {
     json.username,
   ]);
 
-  console.log(res.rows[0]);
-  return NextResponse.json({ data: res.rows[0] });
+  if (res.rowCount == 0) return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  const user = res.rows[0];
+
+  const match = await compare(json.password, user.password);
+
+  if (!match) return NextResponse.json({ error: 'Invalid credentials' }, { status: 401 });
+
+  const token = await new SignJWT({})
+    .setProtectedHeader({ alg: 'HS256' })
+    .setSubject(user.id)
+    .setIssuedAt()
+    .setExpirationTime('2w')
+    .sign(new TextEncoder().encode('token-secret'));
+
+  const response = NextResponse.json({ message: 'Login success' });
+
+  // Prevent cors attack
+  response.cookies.set('jwt-token', token, { sameSite: 'strict', httpOnly: true, secure: true });
+
+  return response;
 }
