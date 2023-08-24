2158a71d27ce8b10f55bdcb711217032e9484bb2
Karl <enriquezkharl14@gmail.com>
2023-08-24 12:14:09 +0800

    private layout

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,5 +1,15 @@
 import { ReactNode } from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import Navbar from './Navbar';
 
 export default function PrivateLayout({ children }: { children: ReactNode }) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <Header />
+      <Navbar />
+      {children}
+      <Footer />
+    </main>
+  );
 }
