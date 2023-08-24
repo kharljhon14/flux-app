e3f1cff09002403516af707cb8da7332d001a21f
Karl <enriquezkharl14@gmail.com>
2023-08-24 21:41:14 +0800

    swr config

--- src/app/(private)/Header.tsx
+++ src/app/(private)/Header.tsx
@@ -1,3 +1,14 @@
+'use client';
+
+import useSWR from 'swr';
+
 export default function Header() {
-  return <header>Header</header>;
+  const { data, error, isLoading } = useSWR('api/users/profile');
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(data);
+
+  return <header>{data.data.username}</header>;
 }
--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,15 +1,21 @@
+'use client';
+
 import { ReactNode } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Navbar from './Navbar';
+import { SWRConfig } from 'swr';
+import fetcher from '@/utils/fetcher';
 
 export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
-    <main>
-      <Header />
-      <Navbar />
-      {children}
-      <Footer />
-    </main>
+    <SWRConfig value={{ fetcher: fetcher }}>
+      <div>
+        <Header />
+        <Navbar />
+        {children}
+        <Footer />
+      </div>
+    </SWRConfig>
   );
 }
