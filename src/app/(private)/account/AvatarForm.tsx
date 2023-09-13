e35011d63384c47897c0a59f3857d283c606b38a
kharle14 <kharl.jhon_enriquez@questronix.com.ph>
2023-09-13 13:11:51 +0800

    avatart form

---
+++ src/app/(private)/account/AvatarForm.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { User } from '@/types';
+import useSWR from 'swr';
+
+import Image from 'next/image';
+
+export default function AvatarForm() {
+  const { data, error, isLoading } = useSWR('/api/users/profile');
+
+  if (error) return <div>Failed to load</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+
+  const user: User = data.data;
+
+  return (
+    <form action="">
+      {user.avatar ? (
+        <div>
+          <Image
+            src={user.avatar}
+            alt={user.avatar}
+            width={200}
+            height={200}
+            style={{ width: 'auto', height: 'auto' }}
+          />
+        </div>
+      ) : (
+        <div className="bg-gray-600 rounded-full m-auto my-5 h-[200px] w-[200px]"></div>
+      )}
+      <input type="file" />
+    </form>
+  );
+}
---
+++ src/app/(private)/account/page.tsx
@@ -0,0 +1,10 @@
+import AvatarForm from './AvatarForm';
+
+export default function AccountPage() {
+  return (
+    <main>
+      <h1>Account</h1>
+      <AvatarForm />
+    </main>
+  );
+}
